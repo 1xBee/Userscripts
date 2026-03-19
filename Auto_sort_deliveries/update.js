4cd5d74b3a9402c754ed9f7a9dda387fea69ebfa
1xBee <184531595+1xBee@users.noreply.github.com>
2026-03-19 15:08:50 +0000

    User script created: Auto_sort_deliveries

---
+++ Auto_sort_deliveries/update.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Auto sort deliverires on date scheduled desc
+// @version      1.0
+// @description  Clicks the sort header twice on Deliveries page load
+// @author       1xBee
+// @match        https://cm.chasunamallny.com/Deliveries
+// @match        https://cm.chasunamallny.com/Deliveries?*
+// @updateURL    https://raw.githubusercontent.com/1xBee/Userscripts/refs/heads/main/Auto_sort_deliveries/update.js
+// @downloadURL  https://raw.githubusercontent.com/1xBee/Userscripts/refs/heads/main/Auto_sort_deliveries/script.js
+// @grant        none
+// ==/UserScript==
